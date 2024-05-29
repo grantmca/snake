5867780af5e14a1e76b0fd6727d813bb25f2273c
Grant <grantmca2221@gmail.com>
2024-05-28 20:57:19 -0500

    Get basic raylib things in there

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include "deque.h"
+#include "raylib.h"
 #include <raylib.h>
 
-int main () {
-  Deque<int> deq;
-  int a = 6;
-  deq.pushEnd(a);
+int main() {
+
+  int screenWidth = 800;
+  int screenHeight = 800;
+
+  InitWindow(screenWidth, screenHeight, "Snake Game");
+
+  SetTargetFPS(60);
+
+  // Main game loop
+  while (WindowShouldClose() == false) // Detect window close button or ESC key
+  {
+    BeginDrawing();
+
+    EndDrawing();
+  }
 
   return 0;
 }
